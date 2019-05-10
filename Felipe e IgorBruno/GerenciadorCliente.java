17108a09e70a348f9bab78c54b5b0d35af42a175
igorb22 <igorb22@live.com>
2019-05-10 08:14:23 -0300

    add retunr

--- Felipe e IgorBruno/GerenciadorCliente.java
+++ Felipe e IgorBruno/GerenciadorCliente.java
@@ -1,7 +1,7 @@
 public class GerenciadorCliente{
 
 	public void inserirCliente(Cliente c){
-
+		return null;
 	}
 
 	public void alterarCliente(Cliente c){
@@ -14,5 +14,6 @@ public class GerenciadorCliente{
 
 
 	public void consultarCliente(Cliente c){
+	
 	}
 }
