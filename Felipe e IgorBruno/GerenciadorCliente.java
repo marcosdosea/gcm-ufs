f72a62880fc2a7c12f7e86dcd4aa059fb4105057
dornelles08 <31745954+dornelles08@users.noreply.github.com>
2019-05-10 08:16:33 -0300

    Conflito resolvido

--- Felipe e IgorBruno/GerenciadorCliente.java
+++ Felipe e IgorBruno/GerenciadorCliente.java
@@ -1,12 +1,7 @@
 public class GerenciadorCliente{
 
-<<<<<<< HEAD
 	public void inserirCliente(Cliente c){
 		return null;
-=======
-	public void inserir(Cliente cliente){
-
->>>>>>> 48bcd68524281105687f5f2e1ecabe1cb801ca0a
 	}
 
 	public void alterar(Cliente cliente){
