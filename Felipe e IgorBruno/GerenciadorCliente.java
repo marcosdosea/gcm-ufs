f6df9564dce0267003d9483433e66999ef117b8a
natan <natan.goes777@gmail.com>
2019-05-10 08:32:42 -0300

    resolvido o conflito

--- Felipe e IgorBruno/GerenciadorCliente.java
+++ Felipe e IgorBruno/GerenciadorCliente.java
@@ -1,11 +1,7 @@
 public class GerenciadorCliente{
 
 	public void inserirCliente(Cliente c){
-<<<<<<< HEAD
 			retuen null;
-=======
-		return null;
->>>>>>> remotes/origin/master
 	}
 
 	public void alterar(Cliente cliente){
