a1788133279599bde00a116f188f205fbe7980c6
Joao <joaoalves.cs@gmail.com>
2019-05-10 08:07:42 -0300

    alteração no método deletar()

--- IgorJoao/GerenciadorCliente.java
+++ IgorJoao/GerenciadorCliente.java
@@ -3,7 +3,7 @@ public class GerenciadorCliente {
 	public void inserir(Cliente cliente){}
 	public void alterar (Cliente cliente){}
 	public void consultar (Cliente cliente){}
-	public deletar (Cliente cliente){
+	public deletar (Cliente clente){
 		List<Produto> list = new ArrayList<>();
 	}
 }
