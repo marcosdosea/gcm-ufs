4b4521c714fe6b0eea65c48b57d4be9fddd17189
abralvs <abraao1514@gmail.com>
2019-05-21 23:58:44 -0300

    close #99

--- GerenciadorCliente.java
+++ GerenciadorCliente.java
@@ -1,3 +1,27 @@
 public class GerenciadorCliente{
 
+	public void adicionarCliente(Cliente cliente){
+		//PersistenciaCliente perCliente = PersistenciaCliente();
+		//perCliente.adicionarCliente(cliente);
+	}
+
+	public void removerCliente(String cpf){
+		//PersistenciaCliente perCliente = PersistenciaCliente();
+		//perCliente.removerCliente(cpf);
+	}
+
+	public void atualizarCliente(Cliente cliente){
+		//PersistenciaCliente perCliente = PersistenciaCliente();
+		//perCliente.atualizarCliente(cliente);
+	}
+
+	public void buscarCliente(String cpf){
+		//PersistenciaCliente perCliente = PersistenciaCliente();
+		//perCliente.buscarCliente(cpf);
+	}
+
+	public void verificarSaldoCliente(String cpf){
+		//PersistenciaCliente perCliente = PersistenciaCliente();
+		//perCliente.verificarSaldoCliente(cpf);
+	}
 }
