e1782c3ee249af3b1ce44b5f0e231bc86020fe0f
dornelles08 <31745954+dornelles08@users.noreply.github.com>
2019-05-23 09:52:53 -0300

    Criação de 5 métodos na GerenciadorCliente Close #114

--- Felipe/GerenciadorCliente.java
+++ Felipe/GerenciadorCliente.java
@@ -1,3 +1,18 @@
 public class GerencidorCliente{
-	
+
+	public void Inserir(Cliente cliente){
+	}	
+
+	public void Alterar(Cliente cliente){	
+	}	
+
+	public void Remover(Cliente cliente){
+	}	
+
+	public void Buscar(Cliente cliente){
+	}	
+
+	public void Buscar(){
+
+	}
 }
