21bd00920e5827a20407f1a907fb38a96b0389f0
Valeria Barbosa <valeria_sb@outlook.com.br>
2019-06-06 16:09:00 -0300

    gerenciarFuncionario.java Fix #141

---
+++ KamilaValeria/gerenciarFuncionario.java
@@ -0,0 +1,54 @@
+package p2;
+
+public class soma {
+
+	public static void main(String[] args) {
+		int soma =0;
+		for (int i = 12; i <= 50; i+=10) {
+			soma = soma+i;
+		}
+		System.out.println(soma);
+
+	}
+public void inserirCliente(Cliente cliente)
+ {
+	add cliente;	
+
+ }
+public void atualizarCliente (cliente, cliente)
+ {
+        
+		rerturn 0;
+ }
+
+
+public void deletarCliente (cliente, cliente)
+ {
+        
+		rerturn 0;
+ }
+
+
+public void pesquisarCliente (cliente, cliente)
+ {
+        
+		rerturn 0;
+ }
+public void cadastrarCliente (cliente, cliente)
+ {
+        
+		rerturn 0;
+ }
+public void alterarCliente (cliente, cliente)
+ {
+        
+		rerturn 0;
+ }
+public int somar (conta, soma)
+ {
+        
+		rerturn 0;
+ }
+
+
+}
