b687bf5eb0427dfb4e28eddc6fa086c804582d8f
Jefferson Santos <jeff.debian2016@gmail.com>
2019-11-14 07:20:56 -0300

    Prática da aula 02 closes #145

---
+++ Jefferson/TreinandoTernario/TreinandoTernario/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TreinandoTernario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var teste = (2 > 4) ? "sim" : "Não";
+            Console.WriteLine("Hello World!:" + teste);
+            Console.ReadKey();
+        }
+    }
+}
