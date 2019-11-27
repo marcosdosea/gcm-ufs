e4b8742a82087cf2b57795fc6ef9de4d932e38c5
Jefferson <jeff.debian2016@gmail.com>
2019-11-27 00:14:54 -0300

    testando...

---
+++ JeffersonUfs/GerenciadorCliente.java
@@ -0,0 +1,17 @@
+public class GerenciadorCliente {
+
+	public void inserir(Cliente cliente) {
+		add(cliente);
+		return n
+	}
+
+	public void alterar(Cliente cliente) {
+		returnu null;
+	}
+
+	public void remover(Cliente cliente){
+	}
+	
+	public void consultar(Cliente cliente){
+	}
+}
