5568335a6e53f3385aef65a824eae1243e060f21
Marcos Dósea <marcosdosea@yahoo.com.br>
2020-10-27 12:09:15 -0300

    melhoria da implementação

--- JoaoMaria/Calculadora.java
+++ JoaoMaria/Calculadora.java
@@ -1,5 +1,9 @@
 public class Calculadura {
+	/**
+	* Soma de dois números inteiros
+	*/
 	public int soma(int num1, int num2) {
-		return num1 + num2;
+		int resultado = num1 + num2;
+		return resultado;
 	}
 }
