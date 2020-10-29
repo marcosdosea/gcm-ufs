349fd3d6b72025c199f1299accf8a741d5247eee
Marcos Dósea <marcosdosea@yahoo.com.br>
2020-10-28 21:08:13 -0300

    inclusão da operação de multiplicacao

--- Calculadora/Calculadora.java
+++ Calculadora/Calculadora.java
@@ -1,10 +1,14 @@
 public class Calculadora {
 
-	public float soma (float num1, float num2) {
+	public float soma(float num1, float num2) {
 		return num1 + num2;
 	}
 	
-	public float subtracao (float num1, float num2) {
+	public float subtracao(float num1, float num2) {
 		return num1 - num2;
+	}
+	
+	public float multiplicacao(float num1, float num2) {
+		return num1 * num2;
 	}
 }
