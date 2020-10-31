1e53d7f2281b03483f75adc8bd4085a4bf7cac5c
Marcos Dósea <marcosdosea@yahoo.com.br>
2020-10-30 23:01:21 -0300

    Resolve #192

--- Calculadora/Calculadora.java
+++ Calculadora/Calculadora.java
@@ -3,7 +3,9 @@ public class Calculadora {
 	public float soma(float num1, float num2) {
 		return num1 + num2;
 	}
-	
+	/**
+	* realiza subtracao
+	*/
 	public float subtracao(float num1, float num2) {
 		return num1 - num2;
 	}
