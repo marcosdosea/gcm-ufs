dae2d7f0081e228a5b3e7ce6154961338bb8bb9d
isLuizMoitinho <luizcarlos_costam@hotmail.com>
2020-11-01 18:39:16 -0300

    #196 - Criação da classe Calculadora_MTO com as operações aritméticas basicas (+, -, / e *)

---
+++ Calculadora_MTO/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Calculadora_MTO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Operações com a calculadora");
+            Calculadora_MTO calculadora = new Calculadora_MTO();
+            System.Console.WriteLine("{0} + {1} = {2}", 2, 5, calculadora.soma(2, 5));
+            System.Console.WriteLine("{0} - {1} = {2}", 2, 5, calculadora.subtracao(2, 5));
+            System.Console.WriteLine("{0} / {1} = {2}", 150, 5, calculadora.divisao(2, 5));
+            System.Console.WriteLine("{0} * {1} = {2}", 2, 5, calculadora.multiplicacao(2, 5));
+
+        }
+    }
+
+    class Calculadora_MTO
+    {
+        protected int firstParam { get; set; }
+        protected int secondParam { get; set; }
+
+        protected int result;
+
+        public int soma(int param1, int param2)
+        {
+            this.result = param1 + param2;
+            return this.result;
+        }
+
+        public int subtracao(int param1, int param2)
+        {
+            this.result = param1 - param2;
+            return this.result;
+        }
+
+
+        public int divisao(int param1, int param2)
+        {
+            if (param2 > 0)
+            {
+                this.result = param1 + param2;
+
+            }
+            else
+            {
+                this.result = 0;
+                System.Console.WriteLine("ERRO: Não é possivel realizar diviões por zero.");
+            }
+
+            return this.result;
+
+        }
+
+        public int multiplicacao(int param1, int param2)
+        {
+            this.result = param1 * param2;
+            return this.result;
+        }
+
+
+
+    }
+}
