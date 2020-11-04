6eef306332402fe009b9a896262e1df26008d476
isLuizMoitinho <luizcarlos_costam@hotmail.com>
2020-11-04 00:24:46 -0300

    #212 - adição da classe DAOCliente e metodo para remoção de cliente

---
+++ LuizMoitinho/DAOCliente.java
@@ -0,0 +1,12 @@
+public class DAOCliente {
+    Boolean save;
+    public Boolean delete(Cliente cliente){
+        /**
+         * codigo para remover um cliente
+         * 
+         */
+        return save;
+    }
+
+    
+}
