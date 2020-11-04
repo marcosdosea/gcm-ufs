cbacdbbd2ed783dd3799e5b0cbbe4b8ab4148e2f
Gilson Caio <gilsoncaio7@gmail.com>
2020-11-04 00:24:56 -0300

    #213 - Adicao da funcionalidade de remocao de cliente

--- GilsonCaio/GerenciadorCliente.java
+++ GilsonCaio/GerenciadorCliente.java
@@ -2,9 +2,15 @@ public class GerenciadorCliente{
   protected int id;
   protected String nome;
 
+  private DAOCliente daoCliente = new DAOCliente();
+
   public int inserir(Cliente cliente) {
     this.id = cliente.id;
     this.nome = cliente.nome;
     return 1;
+  }
+
+  public int remover(Cliente cliente){
+    daoCliente.delete(cliente)
   }
 }
