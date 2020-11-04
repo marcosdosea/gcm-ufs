af9f716df8709f9ea45f838f59ad38676ba4e790
isLuizMoitinho <luizcarlos_costam@hotmail.com>
2020-11-04 00:33:00 -0300

    #211 - rotina para atualizar dados de uma venda

---
+++ LuizMoitinho/DAOVenda.java
@@ -0,0 +1,11 @@
+public class DAOVenda {
+    Boolean save;
+    public Boolean update(GerenciadoraVenda venda){
+        /**
+         * codigo para atualziar uma venda
+         * 
+         */
+        return save;
+    }
+    
+}
