c9b3732ccae3f52936b330064f754f62c1dd0071
Henrique Prado <henrique.mxnero@gmail.com>
2020-11-04 10:12:46 -0300

    método alterar dados resolvido

--- HenriquePrado/GerenciadorCliente.java
+++ HenriquePrado/GerenciadorCliente.java
@@ -1,6 +1,14 @@
 public class GerenciadorCliente {
-	public int inserir(Cliente cliente) {
-		// o código não precisa compilar. É apenas para exercitar a inclusão de linhas de código
+	public int alterarDados(Cliente cliente) {
+		StringBuilder sql = new StringBuilder("UPDATE  cliente SET ");
+		sql.append("nome = '").append(Jnome.getText()).append("',");
+		sql.append("cpf = '").append(Jcpf.getText()).append("',");
+		sql.append("endereco = '").append(Jendereco.getText()).append("',");
+		sql.append("numero = '").append(Jnumero.getText()).append("',");
+		sql.append("cidade = '").append(Jcidade.getText()).append("',");
+		sql.append("cep = '").append(Jcep.getText()).append("',");
+		sql.append("telefone = '").append(Jtelefone.getText()).append("' ");
+		sql.append("WHERE criterio = '").append("campo criterio aqui").append("'")
 	}
 }
 
