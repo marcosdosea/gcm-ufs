cc2947f4f4addd4cfc8f62de863acdce8d420ed4
Dryele <dryele.an@gmail.com>
2020-11-06 14:38:25 -0300

    Metódo efetuarVenda inserido Resolve #243.

--- Dryele/GerenciadorVenda.java
+++ Dryele/GerenciadorVenda.java
@@ -1,8 +1,18 @@
 public class GerenciadorVenda{	
+	float valor;
+	int quantidade;
+	public String produto;
 
 	public int inserir(Cliente cliente){
 		cliente.codigo = 2;
 		cliente.nome = "João"
 	}
 	
+	public int efetuarVenda(float valor, int quantidade, String produto, Cliente cliente){
+		valor = 2.0;
+		quantidade = 1;
+		produto = "Escova";
+		cliente.codigo = 2;
+		cliente.nome = "João"
+	}
 }
