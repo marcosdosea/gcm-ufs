038cb7b09b190280119b37f1ff8e2bdfac2b1a1a
jeovanesf <jeovanesf10@gmail.com>
2020-11-07 17:41:41 -0300

    referente a issue #264

--- Jeovane/GerenciadorVenda.java
+++ Jeovane/GerenciadorVenda.java
@@ -1,5 +1,5 @@
-public class GerenciadorVendas{
-	public int id;
-	public float valor;
-	public String nomeProduto;
+public class GerenciadorVenda{
+	Cliente C;
+	GerenciadorCliente g;
+	g.inserir(c);
 }
