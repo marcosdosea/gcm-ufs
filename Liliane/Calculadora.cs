9b5af15ebd68add502180df30518f75d4a5da139
LilianeCosta767 <lilianecosta767@hotmail.com>
2020-11-06 21:07:07 -0300

    implementacao das classes de venda e clientes

---
+++ Liliane/Calculadora.cs
@@ -0,0 +1,15 @@
+public class GerenciadorCliente(){
+    public int inserir(Cliente cliente) {
+
+        // o código não precisa compilar. É apenas para exercitar a inclusão de linhas de código
+
+    }
+
+}
+
+
+public class GerenciadorVenda() {
+
+    var c = new GerenciadorCliente();
+
+}
