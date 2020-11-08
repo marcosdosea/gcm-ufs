48b8466cba87e805542ff4569407cccc4b796be1
LilianeCosta767 <lilianecosta767@hotmail.com>
2020-11-08 11:46:58 -0300

    implementacao do crud vendedor

--- Liliane/CRUD.cs
+++ Liliane/CRUD.cs
@@ -24,4 +24,22 @@ public class GerenciadorVenda() {
 
     var c = new GerenciadorCliente();
 
+    public int inserir(Vendedor vendedor) {
+
+        // insere Vendedor, logo, create
+
+    }
+
+    public void read(Vendedor vendedor) {
+        // faz a leitura do Vendedor
+    }
+
+    public void update(Vendedor vendedor) {
+        // faz a atualização do Vendedor
+    }
+
+    public void delete(Vendedor vendedor) {
+        // faz a remoção do Vendedor
+    }
+
 }
