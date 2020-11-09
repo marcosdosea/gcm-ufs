478d3f50a143c8206ca449e4c638647cef1e480c
valeria_sb@outlook.com.br <06092018lys>
2020-11-09 18:04:49 -0300

    Implementação do método incluir produto na classe GerenciadorVenda.java #323

--- ValériaBarbosa/GerenciadorVenda.java
+++ ValériaBarbosa/GerenciadorVenda.java
@@ -1,4 +1,7 @@
 public class GerenciadorVenda{
+    public void cadastrarProduto(string nome){
+
+    }
 
     public static void main(String args[]) {
         GerenciadorCliente c = new GerenciadorCliente();
