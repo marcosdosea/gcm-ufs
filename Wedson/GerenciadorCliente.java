c67f06d486b9df012e4a778458eabf5cd6d9b1f5
Wedsonteixeira <wedsonteixeira1998@outlook.com>
2020-11-09 09:40:55 -0300

    resolvendo issue e fechando issue close#298

--- Wedson/GerenciadorCliente.java
+++ Wedson/GerenciadorCliente.java
@@ -1,10 +1,17 @@
 public class GerenciadorCliente{
-
+	private int codigo;
+   	private String nome;
+    	private int cpf;
+    
 	public int inserir(Cliente cliente) {
 	//inserir cliente
 	}
 	
 	public int remover(Cliente cliente) {
 	//inserir cliente
+	}
+
+	public int alteraDados(Cliente cliente) {
+	//altera Dados
 	}
 }
