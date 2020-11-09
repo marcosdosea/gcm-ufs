357c880f2423c33ceeea4f0a16ad353f7866cb65
Rafael Silveira de Andrade <rafaelsilveirarafa@gmail.com>
2020-11-09 17:13:28 -0300

    classes gerenciadorVendas e gerenciadorCliente

---
+++ Rafael Silveira/gerenciadorCliente.java
@@ -0,0 +1,16 @@
+public class Cliente {
+    private String nome;
+    private String CPF;
+    private int codigo;
+
+    public int inserir(String nome, String CPF) {
+        this.nome = nome;
+        this.CPF = CPF  
+	this.codigo = new Random().nextInt();      
+    }
+
+    public Cliente alterarCliente(String nome,int cpf){
+        this.nome = cliente.nome;
+        this.cpf = cliente.CPF;
+    }
+}
