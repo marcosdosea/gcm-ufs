63602aae153d6e023f95988871dad7d5067c6fed
valeria_sb@outlook.com.br <06092018lys>
2020-11-09 18:55:33 -0300

    Implementado o método cadastrar produto #323

--- ValériaBarbosa/GerenciadorVenda.java
+++ ValériaBarbosa/GerenciadorVenda.java
@@ -1,5 +1,6 @@
 public class GerenciadorVenda{
-   
+   public void cadastrarProduto(String nome){
+   }
     public static void main(String args[]) {
         GerenciadorCliente c = new GerenciadorCliente();
         c.inserirCliente("Miguel");
