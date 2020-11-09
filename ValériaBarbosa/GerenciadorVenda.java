788b3d0d64507baaced0addccef82f449d7fdecb
valeria_sb@outlook.com.br <06092018lys>
2020-11-09 17:53:29 -0300

    Classe GerenciadorVenda.java chamando um método da classe GerenciadorCliente #320

---
+++ ValériaBarbosa/GerenciadorVenda.java
@@ -0,0 +1,7 @@
+public class GerenciadorVenda{
+
+    public static void main(String args[]) {
+        GerenciadorCliente c = new GerenciadorCliente();
+        c.inserirCliente("Miguel");
+    }
+}
