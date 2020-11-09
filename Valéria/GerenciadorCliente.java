3dafccaba68f55dea832386a9df9551faaab9610
valeria_sb@outlook.com.br <06092018lys>
2020-11-09 10:25:14 -0300

    Método de excluir implementado #235

---
+++ Valéria/GerenciadorCliente.java
@@ -0,0 +1,10 @@
+public class GerenciadorCliente{
+  public int excluirCliente(int cdg){
+      if(cdg ==codigo){
+         cliente[cdg] = null
+      }else{
+         return -1;
+       }
+   }
+
+}
