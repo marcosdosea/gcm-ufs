15ce7d32d84b0536caa0c15096c8a564ac54c240
Kamila Lima <mila.a.c.lima@hotmail.com>
2020-11-09 22:50:26 -0300

    Foi criado o construtor gerenciadorVenda #360

--- Kamila/GerenciadorVenda.java
+++ Kamila/GerenciadorVenda.java
@@ -1,3 +1,11 @@
 public class GerenciadorVenda {
-    
+
+
+    public GerenciadorVenda() {}
+
+    public GerenciadorVenda(int id, Cliente cliente) {
+        id = id;
+        cliente = cliente;
+    }
+
 }
