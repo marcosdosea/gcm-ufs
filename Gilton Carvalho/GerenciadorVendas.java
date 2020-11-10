69d71e028cbfe751bbd106763863cccaa12b064a
Gilton Carvalho <gilton.carvalho.gcs@gmail.com>
2020-11-09 21:40:25 -0300

    Foi Criado a Classe Gerenciador Vendas

---
+++ Gilton Carvalho/GerenciadorVendas.java
@@ -0,0 +1,9 @@
+public class GerenciadordeVendas {
+    public int adicionarVenda(Cliente cliente, int valor) {
+      
+    }
+	
+    public int removerVenda(Cliente cliente, int codigo)    {
+
+    }
+}
