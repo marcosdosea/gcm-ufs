29acb65aa297ea871ec5b0ca563457b6b3439b0f
Gilton Carvalho <gilton.carvalho.gcs@gmail.com>
2020-11-09 21:38:39 -0300

    Foi Criado a Classe Gerenciador Cliente

---
+++ Gilton Carvalho/GerenciadorCliente.java
@@ -0,0 +1,11 @@
+public class GerenciadorCliente{
+	public void inserir(Cliente cliente){ 
+		 
+	} 
+	public void Remover(Cliente cliente) { 
+
+	} 
+	public void Atualizar(Cliente cliente) { 
+
+	}
+}
