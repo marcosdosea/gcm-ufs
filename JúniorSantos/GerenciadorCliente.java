24692ca737e3a2691761366f4befe161f1f645af
JrSts <jrs.ifs@gmail.com>
2021-01-19 00:20:59 -0300

    Praticando Gerência de Configuração de Software

---
+++ JúniorSantos/GerenciadorCliente.java
@@ -0,0 +1,9 @@
+public class GerenciadorCliente{
+
+	public int inserir(Cliente cliente) {
+		if (cliente !in clientes)
+			clientes = cliente;
+		else
+			system.out.println("cliente ja cadastrado")
+	}
+}
---
+++ JúniorSantos/GerenciadorVenda.java
@@ -0,0 +1,7 @@
+public class GerenciadorVenda{
+
+	public int main(String[] args) {
+		gerenciadorCliente = new GerenciadorCliente();
+		gerenciadorCliente.inserir()
+	}
+}
