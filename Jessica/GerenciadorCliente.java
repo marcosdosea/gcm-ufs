c5b3cb667e524e1578f76944c10199967b81d0ec
JessicaPortilio <jessicaket91@gmail.com>
2021-09-14 16:14:51 -0300

    Tive que remover para fazer tudo de novo

--- Jessica/GerenciadorCliente.java
+++
@@ -1,9 +0,0 @@
-class GerenciadorCliente {
-    public int inserir(Cliente cliente) {
-        ClienteDAO.insert(cliente);
-    }
-
-    public int consultar(Cliente cliente) {
-        return ClienteDAO.consult(cliente);
-    }
-}
