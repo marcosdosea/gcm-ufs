15ab17ba45ee7cc71eb26b574a4f1b7766dbc756
JessicaPortilio <jessicaket91@gmail.com>
2021-09-14 16:21:22 -0300

    Foi adicionado na Classe GerenciadorCliente os métodos remover e editar conforme pedido na issues #444

--- Jessica/GerenciadorCliente.java
+++ Jessica/GerenciadorCliente.java
@@ -6,4 +6,12 @@ class GerenciadorCliente {
     public int consultar(Cliente cliente) {
         return ClienteDAO.consult(cliente);
     }
+
+    public int remover(Cliente cliente) {
+        return ClienteDAO.remove(cliente);
+    }
+
+    public int editar(Cliente cliente) {
+        return ClienteDAO.edit(cliente);
+    }
 }
