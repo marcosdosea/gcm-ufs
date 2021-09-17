7600e91d6f7ed02b539c50655dec4a7737722c19
Victor <victorluz.lima@gmail.com>
2021-09-17 20:05:01 -0300

    create class GerenciadorCliente #481

---
+++ Victor_Lima/GerenciadorCliente.java
@@ -0,0 +1,9 @@
+//Created Victor Lima
+public class GerenciadorCliente{
+    void insereCliente (Object Cliente){
+        //metodo aqui
+    }
+    <Cliente> getCliente (int idCliente){
+        //metodo aqui
+    }
+}
