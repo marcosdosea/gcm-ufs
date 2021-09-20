ff79bb8aaf9d302c709004ea2d0063e6f9accf1e
Pedro Antônio Santos Lima <pedrosl1-@hotmail.com>
2021-09-20 20:21:17 -0300

    Resolvendo issue #507

--- Pedro Antonio/GerenciadorCliente.java
+++ Pedro Antonio/GerenciadorCliente.java
@@ -1,4 +1,11 @@
 public class GerenciadorCliente() {
+    public void inserir_cliente(String nome){
+
+    }
+
+    public void consultar_cliente(String nome){
+
+    }
     public void remover_cliente(String nome){
 
     }
