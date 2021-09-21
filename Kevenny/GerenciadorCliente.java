29bfdbd09ca458b784794c91d602182a2b368ee2
KevennyJS <kevennykeke@gmail.com>
2021-09-20 22:23:04 -0300

    Adicionando gerenciador de cliente relativo a issue #500

---
+++ Kevenny/GerenciadorCliente.java
@@ -0,0 +1,8 @@
+public class GerenciadorCliente{
+    boolean removerCliente (int id){
+        //todo implementar remover Cliente
+    }
+    boolean alterarCliente (Object cliente){
+        //todo implementar alterar cliente
+    }
+} 
