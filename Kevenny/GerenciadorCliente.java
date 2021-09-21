bda2b5bfb7f2158aa1f524785ed9a615d4844971
KevennyJS <kevennykeke@gmail.com>
2021-09-20 22:35:26 -0300

    Criando classe gerenciadorVenda  #501

---
+++ Kevenny/GerenciadorCliente.java
@@ -0,0 +1,15 @@
+public class GerenciadorCliente{
+    boolean inserirCliente (Object cliente){
+        //todo implementar inserir Cliente
+
+    Cliente getCliente (int id){
+        //todo implementar get Cliente
+    }
+    
+    boolean removerCliente (int id){
+        //todo implementar remover Cliente
+    }
+    boolean alterarCliente (Object cliente){
+        //todo implementar alterar cliente
+    }
+} 
