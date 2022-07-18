c60b71be641897b648d95b1555f0272b7a958cff
Marcos Dósea <marcosdosea@yahoo.com.br>
2022-07-18 11:00:49 -0300

    Foi adicionado o comentário javadoc no método. REsolve #669.

--- Dosea2022/Calculadora.java
+++ Dosea2022/Calculadora.java
@@ -1,5 +1,10 @@
 public class Calculadora {
 	
+	/**
+	* Soma primeiro com o segundo número
+	* @param num1 primeiro número
+	* @param num2 segundo número
+	*/
 	public int soma(int num1, int num2) {
 		return num1 + num2;
 	}
