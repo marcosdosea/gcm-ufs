50f1f0adcfd605253640eeff103251a3f5099897
Kendy Ferreira De Oliveira <kendy619.kf@gmail.com>
2022-07-18 22:21:49 -0300

    Método inserir adicionado. Close #674

--- Kendy_Milena/GerenciadorCliente.java
+++ Kendy_Milena/GerenciadorCliente.java
@@ -2,7 +2,9 @@ public class Cliente {
 
   private String nome;
   private int numero;
-
-  
-
+  public static void  Inserir(Cliente cliente) {  
+   if (num% 2 == 0 )   
+      System.out.println (num + "é par" )  
+   else   
+      System.out.println (num + "é ímpar" );  
 }
