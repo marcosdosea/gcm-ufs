4cf665a5d4871538e89e7905e977680643142aa1
Milena <milena20182018@gmail.com>
2022-07-18 22:31:37 -0300

    Atualizando o metodo de inserir e atualizar da classe de cliente.

--- Kendy_Milena/GerenciadorCliente.java
+++ Kendy_Milena/GerenciadorCliente.java
@@ -3,14 +3,11 @@ public class Cliente {
   private String nome;
   private int numero;
 
-  public void atualizar(Cliente cliente) {
-
+  public boolean atualizar(Cliente cliente) {
+    return true;
   }
-  
+
   public static void  Inserir(Cliente cliente) {  
-    if (num% 2 == 0 )   
-      System.out.println (num + "é par" )  
-    else   
-      System.out.println (num + "é ímpar" );  
+    
   }
 }
