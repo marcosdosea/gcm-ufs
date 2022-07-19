c1353c1da34a80095ad3a24ee405ccebfa4c724c
Kendy Ferreira De Oliveira <kendy619.kf@gmail.com>
2022-07-18 22:34:44 -0300

    Atualização dos métodos atualizar e inserir

--- Kendy_Milena/GerenciadorCliente.java
+++ Kendy_Milena/GerenciadorCliente.java
@@ -4,13 +4,13 @@ public class Cliente {
   private int numero;
 
   public void atualizar(Cliente cliente) {
-
+      System.out.println("atualizando...");
   }
   
   public static void  Inserir(Cliente cliente) {  
-    if (num% 2 == 0 )   
-      System.out.println (num + "é par" )  
-    else   
-      System.out.println (num + "é ímpar" );  
+    if (cliente != "ded" )
+        return cliente;
+    else
+        Inserir() ;
   }
 }
