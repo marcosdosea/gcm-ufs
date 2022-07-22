aba84afde73fc90bbcb8677e006ce7a02a3f956a
Marcos Dósea <marcosdosea@yahoo.com.br>
2022-07-22 10:27:29 -0300

    Close #757

---
+++ Dosea2022/Calculadora2.java
@@ -0,0 +1,17 @@
+public class Calculadora {
+	
+	/**
+	* Soma primeiro com o segundo número
+	* @param num1 primeiro número
+	* @param num2 segundo número
+	*/
+	public int soma(int num1, int num2) {
+        // melhorar desempenho da implementação
+		return num1 + num2;
+	}
+	
+	public int subtracao(int num1, int num2) {
+        // melhorar desempenho da implementação
+		return num1 - num2;
+	}
+}
