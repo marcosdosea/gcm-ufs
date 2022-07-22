65b0cf1694f3f9de55f9554b49bc0e63d9ce8596
Milena <milena20182018@gmail.com>
2022-07-22 10:46:39 -0300

    DEDE

---
+++ r/GerenciadorCliente.java
@@ -0,0 +1,18 @@
+public class GerenciadorCliente{
+
+    int id;
+    String nome;
+    
+    public int inserir(Cliente cliente) {
+        this.id = cliente.id;
+        this.nome = cliente.nome;
+    }
+}
+
+  public void atualizar_cliente(Cliente cliente){
+
+} 
+
+  public void remover_cliente(Cliente cliente){
+
+}
