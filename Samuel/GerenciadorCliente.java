e52b591d2d931c18084703e65bdccfc76170e881
samuel <samuelanjos@academico.ufs.br>
2022-07-27 20:24:30 -0300

    Classe gerenciadorVendas e GerenciadorCliente criadas

---
+++ Samuel/GerenciadorCliente.java
@@ -0,0 +1,8 @@
+
+public class GerenciadorCliente {
+
+    public static void main(String[] args) {
+        System.out.println("oi");
+    }
+    
+}
---
+++ Samuel/gerenciadorVendas.java
@@ -0,0 +1,3 @@
+public class verenciadorVendas{
+  system.out.prinln("Vendido")
+}
