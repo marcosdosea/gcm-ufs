f64122dd8a24dc73dd01111ad841c70eb45cf3a7
Lauro Santana Silva <bestlaurobr@gmail.com>
2022-07-26 21:33:05 -0300

    Metodos adicionados Resolve #811

---
+++ Lauro/GerenciadorCliente.java
@@ -0,0 +1,12 @@
+
+public class GerenciadorCliente{
+    
+    boolean inserirCliente (Object Client){
+        /*codigos aqui */
+    }
+    Object consultarCliente (int idClient){
+        /*codigos aqui */
+        return this;
+    }
+
+}
