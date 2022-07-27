c474c936e077e3fbcdc6adf6043e4089fe34a8fd
Lauro Santana Silva <bestlaurobr@gmail.com>
2022-07-26 21:39:12 -0300

    Metodos adicionados Resolve #812

---
+++ Lauro/GerenciadorCliente.java
@@ -0,0 +1,14 @@
+package Lauro;
+
+public class GerenciadorCliente {
+    
+    boolean removeCliente(int idCliente){
+        /*código aqui */
+        return true;
+    }
+
+    boolean alteraCliente(int idCliente){
+        /*código aqui */
+        return true;
+    }
+}
