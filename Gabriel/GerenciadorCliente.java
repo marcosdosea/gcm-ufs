be1384e9a9fa43ce32f532d7b5f180c095e6d1c0
Gabriel dos Santos <galeraxxx76@gmail.com>
2022-07-29 09:24:29 -0300

    A classe GerenciadorCliente.java foi alterada com sucesso, sendo que dois métodos foram adicionados: o método remover() e alterar(String name). Close #827

--- Gabriel/GerenciadorCliente.java
+++ Gabriel/GerenciadorCliente.java
@@ -1,13 +1,35 @@
 class GerenciadorCliente{
     private String name;
         
+    public GerenciadorCliente{
+        name = " ";
+    }
+
     //Inseri o nome para o gerenciador de cliente
-    public void inserir(String name){
-        this.name = name;
+    public boolean inserir(String name){
+
+        if(name != " ") {
+            this.name = name;
+            return true;
+        }
+
+        return false;
     }
 
     //Retorna o nome do responsável por gerenciar os clientes
     public void consultar() {
         System.out.println("Nome do gerenciador de clientes: " + name);
+    }
+
+    // Apaga nome do gerenciador de clientes
+    public boolean remover(){
+        name = " ";
+        return true;
+    }
+
+    // Altera o nome do gerenciador de clientes
+    public boolean alterar(String name) {
+        this.name = name;
+        return true
     }
 }
