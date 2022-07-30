7d39693391dac7e70478f2c50f111a4c941286ba
charlescosta1 <charlescostapvd@gmail.com>
2022-07-30 10:48:23 -0300

    Resolve #874

---
+++ Charles/GerenciadorCliente.java
@@ -0,0 +1,9 @@
+public class GerenciadorCliente{
+    public static void main(String[] args) {
+        System.out.println("Olá, mundo!");
+    }
+    public void inserir(){
+    }
+    public void consultar(){
+    }
+}
