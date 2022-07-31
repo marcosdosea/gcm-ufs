68fa33080524c3c6e71cc7d02b11fbfc622cbb58
Charles Costa <68356054+charlescosta1@users.noreply.github.com>
2022-07-31 15:58:04 -0300

    Resolve #933

---
+++ Charles/GerenciadorCliente.java
@@ -0,0 +1,15 @@
+public class GerenciadorCliente{
+
+    public String name;
+    public String address;
+
+    public void inserir(String name, String address){
+        setName(name);
+        setAddress(address);
+        System.out.println("Inserido com sucesso.");
+    }
+    public void consultar(){
+        System.out.println("Nome do Cliente.");
+    }
+    
+}
