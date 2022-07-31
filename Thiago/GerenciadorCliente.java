5eac3712ac380c05a0b76bb0345dbc99e801d4a9
xxthiagoboyxx <thiagoboy34@gmail.com>
2022-07-31 12:49:34 -0300

    Métodos de alterar e remover adicionados. Resolve #917

--- Thiago/GerenciadorCliente.java
+++ Thiago/GerenciadorCliente.java
@@ -21,5 +21,17 @@ public class GerenciadorCliente {
         System.out.println("Idade: "+idade);
         System.out.println(String.format("Saldo: %.2f",saldo));
     }
+
+    public void alterar(String nome, Integer idade, double saldo){
+        this.nome = nome;
+        this.idade = idade;
+        this.saldo = saldo;
+    }
+
+    public void remover(){
+        this.nome = null;
+        this.idade = null;
+        this.saldo = 0;
+    }
     
 }
