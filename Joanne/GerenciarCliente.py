0a746c333eac224be9f5946ff1211466a0a5e972
Joanne Stephany PC <joannestephany@gmail.com>
2022-07-31 12:51:50 -0300

    Gerenciar cliente com inserir, consultar, remover e alterar
    Só pra ter certeza que as alterações foram enviadas

--- Joanne/GerenciarCliente.py
+++ Joanne/GerenciarCliente.py
@@ -2,12 +2,9 @@ class GerenciarCliente:
 
     def __init__(self):
         self.clientes = []
-        self.clientes.append(Cliente('João', '12345678901', '
-        Rua dos Bobos, 0'))
-        self.clientes.append(Cliente('Maria', '12345678902', '
-        Rua das Flores, 0'))
-        self.clientes.append(Cliente('Pedro', '12345678903', '
-        Rua das Mangas, 0'))
+        self.clientes.append(Cliente('João', '12345678901', 'Rua dos Bobos'))
+        self.clientes.append(Cliente('Maria', '12345678902', 'Rua das Flores'))
+        self.clientes.append(Cliente('Pedro', '12345678903', 'Rua das Mangas'))
         
     def consultar_clientes(self):
         for cliente in self.clientes:
@@ -19,4 +16,24 @@ class GerenciarCliente:
         endereco = input('Endereço: ')
         self.clientes.append(Cliente(nome, cpf, endereco))
 
+    def remover_cliente(self):
+        cpf = input('CPF: ')
+        for cliente in self.clientes:
+            if cliente.cpf == cpf:
+                self.clientes.remove(cliente)
+                print('Cliente removido com sucesso!')
+                return
+        print('Cliente não encontrado!')
+    
+    def alterar_cliente(self):
+        cpf = input('CPF: ')
+        for cliente in self.clientes:
+            if cliente.cpf == cpf:
+                nome = input('Nome: ')
+                endereco = input('Endereço: ')
+                cliente.nome = nome
+                cliente.endereco = endereco
+                print('Cliente alterado com sucesso!')
+                return
+        print('Cliente não encontrado!')
     
