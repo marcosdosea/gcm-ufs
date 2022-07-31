f558c07d27bf4d46ad450b19aace0d12572c704a
Kendy Ferreira De Oliveira <kendy619.kf@gmail.com>
2022-07-31 14:05:59 -0300

    ADD:: Código funcional

--- Kendy/GerenciadorVenda.java
+++ Kendy/GerenciadorVenda.java
@@ -1,11 +1,10 @@
-/**
- * GerenciadorVenda
- */
 public class GerenciadorVenda {
     public void call() {
         GerenciadorCliente gercliente = new GerenciadorCliente();
         gercliente.Inserir();
+        gercliente.Consultar();
+        gercliente.Remover();
+        gercliente.Alterar();
     }
-    
 }
 
