cb547daff8b561dd664f1e674347efd27fb8c75a
Éricles Dos Santos <67772327+Ericles-Porty@users.noreply.github.com>
2022-07-31 11:35:35 -0300

    Fix:: A issue solicitou apenas os métodos remover e alterar. Você trabalhou duas requisições em uma única pull request.

--- Ericles/GerenciadorCliente.cs
+++ Ericles/GerenciadorCliente.cs
@@ -13,18 +13,7 @@ public class GerenciadorCliente
         this.Nome = nome;
         this.Salario = salario;
     }
-    public void inserir(Cliente cliente)
-    {
-        Console.WriteLine("Inserindo cliente");
-        Clientes.add(cliente);
-    }
-
-    public Cliente consultar(IQueryable<Cliente> queryClientes)
-    {
-        Console.WriteLine("Consultando cliente");
-        return queryClientes.FirstOrDefault();
-    }
-
+    
     public void remover(Cliente cliente)
     {
         Console.WriteLine("Removendo cliente");
