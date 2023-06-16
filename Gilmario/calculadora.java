416181f0bf52fe10108fb866a5b76500257cf57b
haendelh <haendelh@gmail.com>
2023-06-15 21:06:22 -0300

    Implementação da calculadora.

--- Gilmario/calculadora.java
+++ Gilmario/calculadora.java
@@ -1,4 +1,17 @@
 public class Calculadora{
+	public float soma(float a, float b){
+		return a+b;
+	}
 
+	public float subtracao(float a, float b){
+		return a-b;
+	}
 
+	public float multiplicacao(float a, float b){
+		return a*b;
+	}
+
+	public float divisao(float a, float b){
+		return b? a/b : b;
+	}
 }
