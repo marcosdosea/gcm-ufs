43d2ac23cf55a688ae65732942b09f04987ddba7
paulovsnts <paulovitormengao2015@gmail.com>
2023-06-23 10:58:39 -0300

    Adicionados as classes e métodos necessários.

---
+++ PauloV_Marcos/GerenciadorPagamentos.java
@@ -0,0 +1,9 @@
+public class GerenciadorPagamentos{
+    public void registrar(float pagamento, String tipo){
+
+                                  }           
+    public void descontar(float pagamento, String tipo){
+
+                                   }
+
+}
