e9c1d0242f160558cc84a63677649da3e16e8704
Marcos Venicio <mkibutsuji4@gmail.com>
2023-06-23 10:18:13 -0300

    Criado os métodos Inserir e Atualizar

---
+++ PauloV_Marcos/GerenciadorCliente.java
@@ -0,0 +1,10 @@
+public class GerenciadorCliente{
+    public void Inserir(Cliente C){
+
+                                  }           
+    public void Atualizar(Cliente C){
+
+                                   }
+
+}
+
