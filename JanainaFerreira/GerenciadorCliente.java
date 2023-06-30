905ffa2719541afff8af63d5fff2f933b68f0ca0
Janaina Ferreira <janainaferreira53@hotmail.com>
2023-06-30 16:30:45 -0300

    Criado GerenciadorCliente. Resolved #1070
    Classe com métodos inserir e consultar.

---
+++ JanainaFerreira/GerenciadorCliente.java
@@ -0,0 +1,9 @@
+public class GerenciadorCliente{
+    public Cliente inserir(Cliente cliente){
+        return null;
+    }
+
+    public Cliente consultar(String cpf){
+        return null;
+    }
+}
