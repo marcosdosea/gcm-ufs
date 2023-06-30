e3e1b54c2e9ad917c1ed832cba6de6e1f38a9334
Janaina Ferreira <janainaferreira53@hotmail.com>
2023-06-30 16:37:14 -0300

    Adicionados métodos remover e inserir. Resolved #1071
    Métodos adicionados na classe GerenciadorCliente

---
+++ JanainaFerreira/GerenciadorCliente.java
@@ -0,0 +1,10 @@
+public class GerenciadorCliente{
+
+    public void removerCliente(Cliente cliente){
+        //remover cliente
+    }
+
+    public void alterarCliente(Cliente cliente){
+        //alterar cliente
+    }
+}
