6717e46aa25af3192b1800d46265fc0c98c0f48e
haendelh <haendelh@gmail.com>
2023-06-30 21:25:20 -0300

    Adicionado GerenciadorCliente. Resolve #1082

---
+++ HaendelHernan/GerenciadorCliente.java
@@ -0,0 +1,14 @@
+public class GerenciadorCliente{
+    public static void main(String[] args) {
+        adicionarCliente();
+        System.out.println("Cliente adicionado.");
+        consultarCliente();
+    }
+
+    public static void adicionarCliente(){
+        System.out.println("Adicionando cliente...");
+    }
+    public static void consultarCliente(){
+        System.out.println("Dados do cliente:");
+    }
+}
