fe883e480458ad2b54702e24b3428cdef5b1e956
paulovsnts <paulovitormengao2015@gmail.com>
2023-07-03 15:20:05 -0300

    Adicionados os métodos remover e alterar na classe GerenciadorCliente. Resolved #1116

--- PauloVitor/GerenciadorCliente.java
+++ PauloVitor/GerenciadorCliente.java
@@ -8,11 +8,25 @@ public class GerenciadorCliente{
         c.nome = nome;
         c.cpf = cpf;
         c.codigo = codigo;
+        return c;
     }
 
     public void consultar(Cliente c){
         System.out.println(c.nome);
-        System.out.println(c.cpf;
+        System.out.println(c.cpf);
         System.out.println(c.codigo);
+    }
+
+    public void remover(Cliente c){
+        c.nome = null;
+        c.cpf = null;
+        c.codigo = null;
+    }
+
+    public Cliente alterar(Cliente c, String nomeNovo, String cpfNovo, int codigoNovo){
+        c.nome = nomeNovo;
+        c.cpf = cpfNovo;
+        c.codigo = codigoNovo;
+        return c;
     }
 }
