39d8eecc45071494f98bbeffae43c4e506bececd
Jadson Tavares <95153192+TSjadness@users.noreply.github.com>
2023-07-03 16:53:08 -0300

    Resolvido a implementaçao #1137

--- JadsonTavares/GerenciadorCliente.java
+++ JadsonTavares/GerenciadorCliente.java
@@ -1,9 +1,9 @@
 public class GerenciadorCliente {
-
+    // metodo inserir cliente
     public inserir(){
         return null;
     }
-
+    // metodo consultar cliente
     public consultar(){
         return null;
     }
