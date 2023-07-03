4a58dce372418028d6679311047afbfcfa7029c7
Natan Vinícius <88415998+NatanVini7@users.noreply.github.com>
2023-07-03 16:04:01 -0300

    Delete NatanVinicius directory

--- NatanVinicius/GerenciadorVenda.java
+++
@@ -1,7 +0,0 @@
-public class GerenciadorVenda{
-    public void Clinte_Venda(){
-        GerenciadorCliente cliente = new GerenciadorCliente();
-        
-        cliente.Alterar_Cliente();
-    }
-}
