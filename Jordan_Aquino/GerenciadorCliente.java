8e194fc9ecac857dc8fe140669109bbb6421354a
jords2 <contato.jordanaquino@gmail.com>
2023-07-03 21:09:33 -0300

    Adicionei os metodos Remover e alterar na classe GerenciadorCliente. RESOLVED#1158

---
+++ Jordan_Aquino/GerenciadorCliente.java
@@ -0,0 +1,16 @@
+public class GerenciadorCliente{
+    public void Inserir(){
+
+    }
+    public void Consultar(){
+        
+    }
+
+    public void Remover(){
+
+    }
+
+    public void Alterar(){
+        
+    }
+}
