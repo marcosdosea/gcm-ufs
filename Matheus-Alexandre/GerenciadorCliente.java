d3fe2682b8da98ce59e615954b4b78bbac82988c
Matheus <matheuz14.al@gmail.com>
2024-07-24 10:35:10 -0300

    feito

--- Matheus-Alexandre/GerenciadorCliente.java
+++ Matheus-Alexandre/GerenciadorCliente.java
@@ -1,8 +1,8 @@
 Public Class {
     Public Inserir(Cliente cliente){
-        //TODO: inserir cliente
+        //TODO: inserido cliente
     }
     Public Atualizar(Cliente cliente){
-        //TODO: atualizar cliente
+        //TODO: atualizado cliente
     }
 }
