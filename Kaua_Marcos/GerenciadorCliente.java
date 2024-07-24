3b9a6ff53e19bb65c38df07f73f8043a1f0bb7dd
imarcossm <viniciusmorais.com@gmail.com>
2024-07-24 10:42:04 -0300

    Alterações nos métodos.

--- Kaua_Marcos/GerenciadorCliente.java
+++ Kaua_Marcos/GerenciadorCliente.java
@@ -3,10 +3,11 @@ public class GerenciadorCliente
     public  void InserirCliente(Cliente cliente)
     {
         //TODO: Inserir cliente na base de dados
+        //testando nova linha de código na implementação
     }
 
     public void AtualizarCliente(Cliente cliente)
     {
-
+        //testando nova linha de código na implementação parte 2
     }
 }
