544d18d39213683e35ff42c08017cd8872d55d25
marcosdosea <marcosdosea@yahoo.com.br>
2024-07-24 09:53:30 -0300

    Atualização de cliente realizada. Close #1281

--- MarcosDosea/GerenciadorCliente.java
+++ MarcosDosea/GerenciadorCliente.java
@@ -3,5 +3,9 @@ public class GerenciadorCliente {
    public void Inserir(Cliente cliente) {
 	    //TODO: insere cliente na base
    }
+   
+   public void Atualizar(Cliente cliente) {
+		//TODO: atualizar cliente na base de dados
+   }
 
 }
