42eaaa3f76c4f570b7c9868373b5f778da276da2
NadsonTelesMendonca <nadsonteles@hotmail.com>
2024-07-24 22:20:24 -0300

    Atualização

--- Luiz_Nadson/Inserir Cliente.java
+++ Luiz_Nadson/Inserir Cliente.java
@@ -1,6 +1,10 @@
 
 
-public class Inserir_cliente(Cliente cliente)
+class Inserir_cliente(Cliente cliente){
+ Public Static Void main(String[] args) {
+  \\inserir cliente
+ }
+
+class atualizar_cliente(
 \\ criar ainda
 
-public Static Void Main
