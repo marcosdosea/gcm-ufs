e7f804966923c2f52709f14958c0a010e54e5c40
marcosdosea <marcosdosea@yahoo.com.br>
2024-07-26 10:47:22 -0300

    implementação de metodod atualização.

--- MarcosDosea/GerenciadorEntrada.java
+++ MarcosDosea/GerenciadorEntrada.java
@@ -1,6 +1,10 @@
 public class GerenciadorEntrada {
 
     public void Inserir() {
-        TODO: INSERIR ENTRADA
+        ///TODO: INSERIR ENTRADA
+    }
+
+    public void Alterar() {
+        //TODO: ALTERAR DADOS DA ENTRADA
     }
 }
