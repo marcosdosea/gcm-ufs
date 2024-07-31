c12aaa7fdb7688a65cf530a302b05fb7a1f1370c
Carol <carolinevivaslins@gmail.com>
2024-07-31 10:16:57 -0300

    Atualizações GerenciadorCalculadora

---
+++ Carol_Lucas/GerenciadorCalculadora.java
@@ -0,0 +1,8 @@
+public class GerenciadorCalculadora{
+    public void inserir(){
+        //TODO: Carol criou a inserir
+    }
+    public void atualizar(){
+        //TODO: Lucas criou essa brincadeira aqui
+    }
+}
