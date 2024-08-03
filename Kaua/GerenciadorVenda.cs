dd26f869161bf5c4e75494139beeccef04f83c05
K4U4dev <okaua057.cod@gmail.com>
2024-08-03 00:28:23 -0300

    Implementado o gerenciamento de vendas, resolve #1381

---
+++ Kaua/GerenciadorVenda.cs
@@ -0,0 +1,15 @@
+namespace GerenciadorVenda
+{
+    public class GerenciadorVenda
+    {
+        public void FinalizarVenda()
+        {
+            // Código para finalizar venda
+        }
+
+        public void CancelarVenda()
+        {
+            // Código para cancelar venda
+        }
+    }
+}
