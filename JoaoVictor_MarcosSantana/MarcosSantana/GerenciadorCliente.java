28d6f7a15a775cdefb166deb626441ba9fdaa459
M4RCOSVS0 <marcossvin.so@gmail.com>
2024-08-05 15:03:27 -0300

    closed #1389

---
+++ JoaoVictor_MarcosSantana/MarcosSantana/GerenciadorCliente.java
@@ -0,0 +1,13 @@
+public class GerenciadorCliente 
+{
+    private void Inserir(Cliente cliente)
+    {
+        //TODO:ggg
+    }
+    private void consultar(Cliente cliente){
+        //TODO:
+    }
+
+
+}
+
