227e95fd76857197a9e0e8e28f75c5e94e100b84
Felip3D3V <143296850+Felip3D3V@users.noreply.github.com>
2024-08-07 16:28:37 -0300

    Revert "adicionado novos metotos na classe GerenciadorClientes."

--- FelipeMendonça/GerenciadorCliente.java
+++ FelipeMendonça/GerenciadorCliente.java
@@ -1,26 +1,16 @@
 nomespace GerenciadorCliente
 {
-public class GerenciadorCliente
-{
-	public void RemoverCliente(Cliente cliente)
+	public class GerenciadorCliente
 	{
-		// Remove o cliente
-	}
+		
+		public void InserirCliente(Cliente cliente)
+		{
+			// Realiza a inserção do cliente
+		}
 
-	public void AtualizarCliente(Cliente cliente)
-	{
-		// Atualiza o cliente
+		public Cliente ConsultarCliente(int id)
+		{
+			// Realiza a consulta do cliente
+		}
 	}
-
-
-	public void InserirCliente(Cliente cliente)
-	{
-		// Realiza a inserção do cliente
-	}
-
-	public Cliente ConsultarCliente(int id)
-	{
-		// Realiza a consulta do cliente
-	}
-}
 }
