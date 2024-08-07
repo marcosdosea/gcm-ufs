05fb32a8dee4df528a6d09e5b921ffa1b3bd09d6
MarcosVinicius-Mota <marcosvinicius.mota652790@gmail.com>
2024-08-07 11:41:36 -0300

    Criação dos métodos inserir e consultar na classe GerenciadorCliente. closed #1461

---
+++ MarcosMota/GerenciadorCliente.java
@@ -0,0 +1,16 @@
+public class GerenciadorCliente{
+
+	public GerenciadorCliente(){ 
+	}
+
+	public Integer inserir(Cliente cliente){
+		//Código de inserir e retornar o Id
+		return 0;
+	}
+
+	public Cliente consultar(int id){
+		//Código de consultar o cliente e retornar os dados
+		return null;
+	}
+
+}
