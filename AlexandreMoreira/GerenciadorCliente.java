fdfd8e6309fa7018d6c38c1c23a4cbd30395a025
Alexandre Moreira <xandemoreira1505@gmail.com>
2024-08-07 15:15:36 -0300

    Implementacao da classe GerenciadorCliente com método de inserir e consultar Referente a ishue Criar a classe GerenciadorCliente com método de inserir e consultar #1453

---
+++ AlexandreMoreira/GerenciadorCliente.java
@@ -0,0 +1,17 @@
+public class GerenciadorCliente{
+
+	public GerenciadorCliente(){ 
+	}
+
+	public Integer inserir(Cliente cliente){
+		
+		return 0;
+	}
+
+	public Cliente consultar(int id){
+		
+		return null;
+	}
+  
+
+}
