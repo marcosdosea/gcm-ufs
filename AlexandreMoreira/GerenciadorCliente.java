b197cbc7c829eb7ac05410e22de8ba7696df5610
Alexandre Moreira <xandemoreira1505@gmail.com>
2024-08-07 15:39:07 -0300

    Foi implementado a classe GerenciadorCliente métodos remover e alterar na issue, Adicionar na classe GerenciadorCliente métodos remover e alterar #1454

---
+++ AlexandreMoreira/GerenciadorCliente.java
@@ -0,0 +1,26 @@
+public class GerenciadorCliente{
+
+	public GerenciadorCliente(){ 
+	}
+
+	public Integer inserir(Cliente cliente){
+		
+		return 0;
+	}
+
+	public Cliente consultar(int id){
+		
+		return null;
+	}
+	public Cliente alterar(Cliente cliente){
+		
+		return cliente;
+	}
+
+	public boolean remover(int id){
+		
+		return false;
+	}
+
+
+}
