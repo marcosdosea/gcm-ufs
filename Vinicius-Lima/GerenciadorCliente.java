412822a4ea1d4ab8e182d42970c206d394db9b08
Vinícius Lima Santos <103907631+Vingreyck@users.noreply.github.com>
2024-08-07 17:52:42 -0300

    Update and rename remove_e_altera.java to GerenciadorCliente.java

---
+++ Vinicius-Lima/GerenciadorCliente.java
@@ -0,0 +1,14 @@
+public class GerenciadorCliente{
+     void inserir(){
+        //TODO:"inserir"
+    }
+    void consultar(){
+        //TODO:"consultar"
+    }
+    void remover(){
+        //TODO:"remover"
+    }
+    void alterar(){
+        //TODO:"alterar"
+    }
+}
