407472365ecc8094e1d5c9b42b32dc59ba6bc61c
CamilaF56 <camila56@academico.ufs.br>
2024-08-06 21:25:31 -0300

    Cria os novos métodos e classe GerenciadorCliente.java.
    Close #1420

---
+++ CamilaFontes/GerenciadorCliente.java
@@ -0,0 +1,11 @@
+public class GerenciadorCliente {
+
+    public void InserirCliente(Cliente cliente) {
+        // TODO: Inserir Cliente
+    }
+
+    public void ConsultarCliente(Cliente cliente) {
+        // TODO: Consultar Cliente
+    }
+
+}
