f0a304bd1624e22f6dce85eb2d509f01cdd88f17
Jonatha Gabriel <jonathagabrielns@gmail.com>
2024-08-07 20:54:56 -0300

    closes #1507

---
+++ JonathaGabriel/GerenciadorCliente.py
@@ -0,0 +1,10 @@
+class GerenciadorCliente:
+    def __init__(self):
+        self.bleh = 'bleh'
+
+    def inserir(self):
+        raise NotImplementedError
+
+    def consultar(self):
+        raise NotImplementedError
+
