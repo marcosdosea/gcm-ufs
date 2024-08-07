308779d7275c753c76b1c1fd79cb509ef76a7ece
Pedro Dantas <pedrood1232@gmail.com>
2024-08-07 17:38:10 -0300

    Closed #1496

---
+++ PedroDantas/GerenciadorVenda.java
@@ -0,0 +1,11 @@
+public class GerenciadorVenda {
+
+    public void finalizar(Venda venda) {
+        // TODO: finalizar venda
+    }
+
+    public void cancelar(Venda venda) {
+        // TODO: cancelar venda
+    }
+
+}
