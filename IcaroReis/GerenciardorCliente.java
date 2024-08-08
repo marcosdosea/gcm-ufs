840f62e5ce420e069f9c390ca856fc02ed557dad
Icaro Reis <85704215+Reismysterr@users.noreply.github.com>
2024-08-07 21:04:01 -0300

    Criar a classe GerenciardorCliente com métodos inserir e consultar.

---
+++ IcaroReis/GerenciardorCliente.java
@@ -0,0 +1,15 @@
+namespace GerenciardorCliente 
+{
+    public class GerenciardorCliente
+    {
+        public void inserir ()
+        {
+            // Código para inserir Cliente
+        }
+
+        public void consultar()
+        {
+            // Código para consultar Cliente
+        }
+    }
+}
