33e563410439d5b9e4d78760d5dea01e356246f7
Jonatha Gabriel <jonathagabrielns@gmail.com>
2024-08-07 22:07:49 -0300

    closes #1509

---
+++ JonathaGabriel/GerenciadorVenda.py
@@ -0,0 +1,10 @@
+class GerenciadorVenda:
+    def __init__(self):
+        self.bleh = 'bleh'
+
+    def finalizar(self):
+        raise NotImplementedError
+
+    def cancelar(self):
+        raise NotImplementedError
+
