e9b066d956c5c21c9f293e53d78704854e50b636
geilson <tom1232012@hotmail.com>
2025-05-30 21:41:55 -0300

    close #1618

--- geilson/GerenciadorFinanceiro.java
+++ geilson/GerenciadorFinanceiro.java
@@ -13,19 +13,19 @@ class Categoria {
     private String nome;
     private String descricao;
 
-    public Categoria(String nome, String descricao) {
+    public Categoria(String nome, String descricao){
         this.nome = nome;
         this.descricao = descricao;
     }
 
     // Getters e Setters
-    public String getNome() { return nome; }
-    public void setNome(String nome) { this.nome = nome; }
+    public String getNome() { return nome;}
+    public void setNome(String nome) { this.nome = nome;}
     public String getDescricao() { return descricao; }
     public void setDescricao(String descricao) { this.descricao = descricao; }
 
     @Override
-    public String toString() {
+    public String toString(){
         return nome + " - " + descricao;
     }
 }
