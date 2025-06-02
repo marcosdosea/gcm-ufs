9e58f798d315e5f6c3facea56f3dae25225edebe
Victor Humgria <humgria.rar@hotmail,com>
2025-06-02 17:35:10 -0300

    close #1629

---
+++ Talysson_João/GerenciadorPagamentos.java
@@ -0,0 +1,15 @@
+public class GerenciadorPagamentos {
+    //implementar
+
+    public void registrarPagamento (){
+        //algo
+    }
+
+    public void processarPagamento (){
+        //algo
+    }
+    public void cancelarPagamento (){
+        //algo
+    }
+
+}
