2c4e23335f72349a05ad03681f31a7ffb3bfc319
Luiz Augusto Farias <sirmaestro007@proton.me>
2025-06-02 11:50:30 -0300

    Declarado classe GerenciadorCliente e definido array de clientes

---
+++ LuizCassiano/GerenciadorCliente.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class GerenciadorCliente {
+    private ArrayList<Cliente> clientes;
+
+    public GerenciadorCliente() {
+        clientes = new ArrayList<>();
+    }
+}
