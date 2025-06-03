4d3452d967ab09421a62e1caf0f64a7f48a066d8
Pedro Dantas <pedrood1232@gmail.com>
2025-06-03 14:36:29 -0300

    Fix metodo inserir #1633

--- Pedro_Henrique/GerenciadorCliente.java
+++ Pedro_Henrique/GerenciadorCliente.java
@@ -4,13 +4,9 @@ public class GerenciadorCliente {
     }
 
     public void inserir(Cliente){
-<<<<<<< HEAD
         //Inserir cliente no sistema - Dev 1
         String nome = "Pedro";
         return null;
-=======
-        //Inserir cliente na base de dados - Dev 2
->>>>>>> cb715b2dfd939e290a1500bde208efd39d779795
     }
 
     public void atualizar(Cliente){
