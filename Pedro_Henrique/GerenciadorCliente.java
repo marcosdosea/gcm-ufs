946202c4516b60796588b0a1a9879a99e4f15eef
Pedro Dantas <pedrood1232@gmail.com>
2025-06-03 14:24:27 -0300

    Implementaçoes metodos do cliente dev1

--- Pedro_Henrique/GerenciadorCliente.java
+++ Pedro_Henrique/GerenciadorCliente.java
@@ -4,9 +4,11 @@ public class GerenciadorCliente {
     }
 
     public void inserir(Cliente){
+        //Inserir cliente no sistema - Dev 1
     }
 
     public void atualizar(Cliente){
+        //Atualizar dados do cliente - Dev 1
     }
 
 }
