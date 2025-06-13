3e0043eeb2794837b4be009c78ed7f076e72e9bb
Rafael S. Lima <rafaelkra2@gmail.com>
2025-06-13 12:07:06 -0300

    Funcionalidade implementada como solicitado. Resolve #1673

---
+++ RafaelLima/GerenciadorCliente.java
@@ -0,0 +1,13 @@
+package RafaelLima;
+
+public class GerenciadorCliente {
+  public boolean inserir(Cliente cliente) {
+    // regras de negócio
+    return true;
+  }
+
+  public boolean consultar(Cliente cliente) {
+    // regras de negócio
+    return true;
+  }
+}
