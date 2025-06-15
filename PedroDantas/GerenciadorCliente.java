7f9343ae47bcec75e150cd764f5249bc8f243adc
Pedro Dantas <pedrood1232@gmail.com>
2025-06-15 11:00:52 -0300

    Impl inicial classe GerenciadorCliente - resolve #1679

---
+++ PedroDantas/GerenciadorCliente.java
@@ -0,0 +1,14 @@
+package PedroDantas;
+
+public class GerenciadorCliente {
+    public boolean inserir(Cliente cliente) {
+        // Inserir novo cliente
+        return true;
+    }
+
+    public Cliente consultar(int id) {
+        // Consultar dados de um cliente
+        return null;
+    }
+
+}
