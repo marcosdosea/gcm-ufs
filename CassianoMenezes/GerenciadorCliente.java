4a69226cbc7fbc16a8e743d232888db8b55078f9
CassianoMn <cassianomenezes1@gmail.com>
2025-06-16 23:42:52 -0300

    Adicionado na classe GerenciadorCliente métodos remover e alterar - closes #1710

--- CassianoMenezes/GerenciadorCliente.java
+++ CassianoMenezes/GerenciadorCliente.java
@@ -6,11 +6,16 @@ public Class GerenciadorCliente {
     }
     
     public void remover(Cliente cliente) {
+        System.out.println("ID: " + cliente.getId());
         System.out.println("Cliente " + cliente.getNome() + " removido com sucesso.");
-    
     }
 
     public void alterar(Cliente cliente) {
+        System.out.println("Novo ID: " + cliente.getId());
+        System.out.println("Novo nome: " + cliente.getNome());
+        System.out.println("Novo email: " + cliente.getEmail());
+        System.out.println("Novo telefone: " + cliente.getTelefone());
+        System.out.println("Novo endereço: " + cliente.getEndereco());
         System.out.println("Cliente " + cliente.getNome() + " alterado com sucesso.");
     }
 
