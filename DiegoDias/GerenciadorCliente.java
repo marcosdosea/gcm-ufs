9d0f9bc771d211cf88dfc8aaef5f6a3e000db6ae
Diego <diego.inocenciio.79@gmail.com>
2025-06-16 23:23:27 -0300

    Adicionado os métodos remover e alterar. Close #1714

---
+++ DiegoDias/GerenciadorCliente.java
@@ -0,0 +1,16 @@
+public class GerenciadorCliente{
+
+	public GerenciadorCliente(){ 
+	}
+
+	public Cliente alterarCliente(Cliente cliente){
+		//Código para alterar o cliente
+		return cliente;
+	}
+
+	public boolean removerCliente(int id){
+		//Código para remover o cliente
+		return false;
+	}
+
+}
