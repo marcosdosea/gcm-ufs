3b344c122747e43bbcbd52adf2aa569cf6cad40f
renneb777 <rennerox46@gmail.com>
2025-06-17 10:13:29 -0300

    Implementado as funcionalidades para remover e alterar. closes 1722

--- RenneBispo/GerenciadorCliente.java
+++ RenneBispo/GerenciadorCliente.java
@@ -7,4 +7,13 @@ public Class GerenciadorCliente {
     public void consultar(Cliente cliente) {
         System.out.println("Consultando cliente: " + cliente.getNome());
     }
+    
+    public void remover(Cliente cliente) {
+        System.out.println("Cliente " + cliente.getNome() + " removido com sucesso.");
+    }
+
+    public void alterar(Cliente cliente) {
+        System.out.println("Cliente " + cliente.getNome() + " alterado com sucesso.");
+    }
+
 }
