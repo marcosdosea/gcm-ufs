402e1b978d468b658e104084db50142f501e9990
luigibarros <luigibarros315@gmail.com>
2025-06-18 20:17:30 -0300

    Foi implementada as funcionalidades de cadastro e consulta de clientes como pedido na Branch #1752

---
+++ LuigiBarros/GerenciadorCliente.java
@@ -0,0 +1,14 @@
+package LuigiBarros;
+
+public class GerenciadorCliente {
+
+    public void inserirCliente() {
+        System.out.println("Cliente inserido.");
+    }
+
+    public void consultarCliente() {
+        System.out.println("Consultando cliente.");
+    }
+}
+
+  
