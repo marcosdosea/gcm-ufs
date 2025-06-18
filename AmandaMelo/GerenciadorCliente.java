f759df360342af3cff5f34d2fa070729a6648c71
Amanda de Jesus Melo <60000299+amandexspeed@users.noreply.github.com>
2025-06-18 11:36:31 -0300

    Implementação da inserção e consulta a clientes
    Close #1746

---
+++ AmandaMelo/GerenciadorCliente.java
@@ -0,0 +1,15 @@
+public class GerenciadorCliente{
+
+    public void inserirCliente() {
+        // Lógica para inserir um cliente
+        System.out.println("Cliente inserido");
+    }
+
+    public void ConsultarCliente() {
+        // Lógica para atualizar um cliente
+        System.out.println("Cliente atualizado");
+    }
+
+
+
+}
