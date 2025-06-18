86c86ba0f4ec41ee05865206662f6b400d9a3d91
luigibarros <luigibarros315@gmail.com>
2025-06-18 20:37:12 -0300

    Foio implementado as funcionalidades que permitem finalizar um venda

---
+++ LuigiBarros/GerenciadorVenda.java
@@ -0,0 +1,13 @@
+package LuigiBarros;
+
+public class GerenciadorVenda {
+
+    public void finalizarVenda() {
+        System.out.println("Venda finalizada com sucesso!");
+    }
+
+    public void cancelarVenda() {
+        System.out.println("Venda cancelada.");
+    }
+
+}
