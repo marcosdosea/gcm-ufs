3f1a6003c410a57b15d34798aa304c61388df3ef
Alanna-Rayssa <alannarayssa21@gmail.com>
2025-10-22 18:39:16 -0300

    Adicionados os arquivos GerenciadorPagamentos.java e GerenciadorVendas.java

---
+++ AlannaHevellyn/GerenciadorPagamentos.java
@@ -0,0 +1,5 @@
+public class GerenciadorPagamentos {
+    public static void main(String[] args) {
+        System.out.println("Arquivo GerenciadorPagamentos criado por Alanna.");
+    }
+}
---
+++ AlannaHevellyn/GerenciadorVendas.java
@@ -0,0 +1,5 @@
+public class GerenciadorVendas {
+    public static void main(String[] args) {
+        System.out.println("Arquivo GerenciadorVendas criado por Alanna.");
+    }
+}
