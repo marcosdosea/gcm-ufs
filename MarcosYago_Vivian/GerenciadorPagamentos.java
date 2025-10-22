4ddbd09dfc6ee5f54b264c67073d961712c1058d
Vivian-SI <vivianherdeira@outlook.com>
2025-10-22 18:16:54 -0300

    Criação do gerenciador de pagamento e vendas de Yago e Vivian

---
+++ MarcosYago_Vivian/GerenciadorPagamentos.java
@@ -0,0 +1,7 @@
+public class GerenciadorPagamentos {
+
+    public static void main(String[] args) {
+       
+    	System.out.println("Bem vindo ao nosso gerenciador de Pagamentos!");
+    }
+}
---
+++ MarcosYago_Vivian/GerenciadorVendas.java
@@ -0,0 +1,7 @@
+public class GerenciadorVendas {
+
+    public static void main(String[] args) {
+       
+    	System.out.println("Bem vindo ao nosso gerenciador de Vendas!");
+    }
+}
