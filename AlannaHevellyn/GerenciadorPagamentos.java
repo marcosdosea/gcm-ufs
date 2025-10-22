19c1294f3d12bac7c7dc2cc8db3b7daab6bc47ec
Alanna Rayssa <156382230+alannarayssaa@users.noreply.github.com>
2025-10-22 19:32:00 -0300

    Update success message in GerenciadorPagamentos

--- AlannaHevellyn/GerenciadorPagamentos.java
+++ AlannaHevellyn/GerenciadorPagamentos.java
@@ -1,5 +1,5 @@
 public class GerenciadorPagamentos {
     public static void main(String[] args) {
-        System.out.println("Arquivo GerenciadorPagamentos criado por Alanna.");
+        System.out.println("Arquivo GerenciadorPagamentos criado com sucesso.");
     }
 }
