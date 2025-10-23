d68e06c1f349ab58a9dbf21b6514d28749b4dfe9
MatheusCalixto42 <matheus76calixto@gmail.com>
2025-10-23 20:06:56 -0300

    Método alterado. Closed #1872

--- Matheus_Paulo/GerenciadorCliente.java
+++ Matheus_Paulo/GerenciadorCliente.java
@@ -1,9 +1,8 @@
 public class GerenciadorCliente {
 
     public void inserir(Cliente cliente) {
-	var _cliente = cliente
-        _cliente.Nome = _cliente.Nome.ToUpper();
-        context.Add(_cliente);
+        cliente.Nome = cliente.Nome.ToUpper();
+        context.Add(cliente);
     }
 
     public void atualizar(Cliente cliente){
