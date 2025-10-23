4524839bd07be97dd0225b309d6a5408de8fa8f8
paulo10 <ph30459@gmail.com>
2025-10-23 12:42:35 -0300

    Adicionado metódo novo para o arquivo java Close #1866

--- Matheus_Paulo/GerenciadorCliente.java
+++ Matheus_Paulo/GerenciadorCliente.java
@@ -5,4 +5,16 @@ public class GerenciadorCliente {
         context.Add(cliente);
     }
 
+    public void atualizar(Cliente cliente){
+        if (cliente == null) return;
+        cliente.Nome = cliente.Nome.toUpperCase();
+        for (int i = 0; i < context.size(); i++) {
+            if (context.get(i).Id == cliente.Id) {
+                context.set(i, cliente);
+                return;
+            }
+        }
+        
+    }
+
 }
