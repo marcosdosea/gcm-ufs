850f19c56b0c4d8e62561dab78b0cf90828bd2d4
MarcosYago17 <yago17marcos@hotmail.com>
2025-10-23 09:06:01 -0300

    Foi criado o método inserir(cliente)

--- MarcosYago_Vivian/GerenciadoCliente.java
+++ MarcosYago_Vivian/GerenciadoCliente.java
@@ -1,5 +1,5 @@
 public class GerenciadorCliente {
-    public static void main(String[] args) {
-        System.out.println("Ta tudo certinho!");
+    public void inserir(cliente)
+        System.out.println("mtodo criado!");
     }
 }
