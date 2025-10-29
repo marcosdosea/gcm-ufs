bbbbba1225c975fc6c16ea2d494f653c5a12f864
Hevellyn De Souza <hevellynalves09@gmail.com>
2025-10-28 21:12:44 -0300

    close #1882

--- AlannaHevellyn/GerenciadorCliente.java
+++ AlannaHevellyn/GerenciadorCliente.java
@@ -2,4 +2,15 @@ public class GerenciadorVendas {
     public static void main(String[] args) {
         System.out.println("Seja bem-vindo(a) ao nosso gerenciador!!!!");
     }
+
+       public int atualizarCliente(int id, String novoNome, String novoEmail) {
+        for (Cliente c : clientes) {
+            if (c.getId() == id) {
+                c.setNome(novoNome);
+                c.setEmail(novoEmail);
+
+            }
+        }
+    }
 }
+
