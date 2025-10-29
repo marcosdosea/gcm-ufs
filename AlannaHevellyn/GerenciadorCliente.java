f607c4372444bbb38d31875fca12c442f06cdfc0
Alanna-Rayssa <alannarayssa21@gmail.com>
2025-10-28 21:12:12 -0300

    Close #1881

--- AlannaHevellyn/GerenciadorCliente.java
+++ AlannaHevellyn/GerenciadorCliente.java
@@ -2,4 +2,8 @@ public class GerenciadorVendas {
     public static void main(String[] args) {
         System.out.println("Seja bem-vindo(a) ao nosso gerenciador!!!!");
     }
+
+    public static void inserirCliente(){
+
+    }
 }
