c428a7b58c5dcde5c2c05b47d0de3ff44228a019
Lorena Pereira Nascimento <lorena.lindaflower@gmail.com>
2025-10-29 22:04:10 -0300

    Método inserido. close #1884

--- LucasCunha_Lorena/GerenciadorCliente.java
+++ LucasCunha_Lorena/GerenciadorCliente.java
@@ -11,4 +11,23 @@ public class GerenciadorCliente{
         }
         
     }
+
+    public Cliente atualizar(int id, String nome_completo,String data_nasc,String cpf){
+
+        Cliente cliente = context.findById(id);
+
+        if (cliente != null){
+            cliente.setNome_completo(nome_completo);
+            cliente.setData_nasc(data_nasc);
+            cliente.setCpf(cpf);
+
+            context.update(cliente);
+
+            return cliente;
+        }else {
+            System.err.println("Erro: Cliente não encontrado");
+            return null;
+        }
+        
+    }
 }
