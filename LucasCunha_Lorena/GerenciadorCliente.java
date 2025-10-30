2cb234f87deed0fd7b5da0bd8f4cd1234dd080a2
LucasCunhaAlmeida <lucascunhaalmeida2003@gmail.com>
2025-10-29 21:50:39 -0300

    Criado o método para inserir novos clientes em meio persistente - closes #1883

--- LucasCunha_Lorena/GerenciadorCliente.java
+++ LucasCunha_Lorena/GerenciadorCliente.java
@@ -1,3 +1,14 @@
 public class GerenciadorCliente{
 
+    public void inserir(String nome_completo,String data_nasc,String cpf){
+
+        if ((nome_completo || data_nasc || cpf) != null){
+            Cliente cliente = new Cliente(nome_completo,data_nasc,cpf);
+
+            context.add(cliente);
+        }else {
+            System.err.println("Erro: Algum campo está vazio");
+        }
+        
+    }
 }
