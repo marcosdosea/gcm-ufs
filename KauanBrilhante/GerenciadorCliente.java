ec2e2f3224066a0b21f8d49f31bdf31eb1f86454
Kauanfbr <kauanfbr10@gmail.com>
2025-11-05 19:38:51 -0300

    Adicionado novos metodos para remover e alterar clientes

--- KauanBrilhante/GerenciadorCliente.java
+++ KauanBrilhante/GerenciadorCliente.java
@@ -3,8 +3,13 @@ public class GerenciadorCliente{
 	public void inserir(){
 		System.Out.Println("Cliente inserido com sucesso!");
 	}
-
 	public void consultar(){
+		System.Out.Println("Cliente consultado com sucesso!");
+	}
+	public void remover(){
+		System.Out.Println("Cliente consultado com sucesso!");
+	}
+	public void alterar(){
 		System.Out.Println("Cliente consultado com sucesso!");
 	}
 }
