fed4d1b95106e2de10f8113702985e5e32d39bbf
Kauanfbr <kauanfbr10@gmail.com>
2025-11-05 19:35:33 -0300

    Adicionando arquivo pra gerenciar clientes

---
+++ KauanBrilhante/GerenciadorCliente.java
@@ -0,0 +1,10 @@
+public class GerenciadorCliente{
+
+	public void inserir(){
+		System.Out.Println("Cliente inserido com sucesso!");
+	}
+
+	public void consultar(){
+		System.Out.Println("Cliente consultado com sucesso!");
+	}
+}
