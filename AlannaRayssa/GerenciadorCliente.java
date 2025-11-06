0e8be8fb04436c924e08b71c08b9457ed83a256b
Alanna Rayssa <156382230+alannarayssaa@users.noreply.github.com>
2025-11-05 22:31:29 -0300

    Add GerenciadorCliente class with basic methods

---
+++ AlannaRayssa/GerenciadorCliente.java
@@ -0,0 +1,13 @@
+public class GerenciadorCliente {
+
+    public GerenciadorCliente() {
+    }
+
+    public boolean inserir(String dadosCliente) {
+        return false; 
+    }
+
+    public String consultar(int idCliente) {
+        return null;
+    }
+}
