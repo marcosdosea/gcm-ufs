1ef361f30bf80ddd6b809980322e6ae6d280e748
João Keweni S R <joaokeweni49@gmail.com>
2025-11-05 21:36:44 -0300

    Classe criada e metodos inseridos

--- João Keweni/GerenciadorCliente.java
+++ João Keweni/GerenciadorCliente.java
@@ -1,16 +1,6 @@
-package João Keweni;
-
-public class GerenciadorCliente {
-    public void inserir(Cliente cliente){
-
-    }
-    public void consultar(Cliente cliente){
-        
-    }
-    public void remover(Cliente cliente){
-
-    }
-    public void alterar(Cliente cliente){
-        
-    }
+public class GerenciadorCliente{
+	public void inserir(Cliente cliente){
+	}
+	public void consultar(Cliente cliente){
+	}
 }
