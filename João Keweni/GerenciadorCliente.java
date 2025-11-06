de147a90b30f1f488c04d2dfdf4e73607af92817
João Keweni S R <joaokeweni49@gmail.com>
2025-11-05 21:27:00 -0300

    metodos remover e alterar inseridos no Gerenciador resolved #1923

--- João Keweni/GerenciadorCliente.java
+++ João Keweni/GerenciadorCliente.java
@@ -7,4 +7,10 @@ public class GerenciadorCliente {
     public void consultar(Cliente cliente){
         
     }
+    public void remover(Cliente cliente){
+
+    }
+    public void alterar(Cliente cliente){
+        
+    }
 }
