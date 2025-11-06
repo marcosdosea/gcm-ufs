d2dfec93979574758e9a12983c8c37b4aac218b5
Vivian-SI <vivianherdeira@outlook.com>
2025-11-06 19:33:18 -0300

    Criação dos métodos inserir e consultar no GerencadorCliente. close#1941

---
+++ Vívian/GerenciadorCliente.java
@@ -0,0 +1,19 @@
+public class GerenciadorCliente {
+
+    public void Cliente(Cliente cliente){
+        //TODO
+    }
+
+    public void Inserir(Cliente cliente){
+        //TODO
+    } 
+
+    public void consultar(Cliente cliente){
+        //TODO
+    }
+
+}
+
+public void main(String[] args){
+    System.out.println("Bem vindo ao Gerenciador");
+}
