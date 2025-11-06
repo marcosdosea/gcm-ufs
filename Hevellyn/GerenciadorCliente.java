84906ff8bc8b83b2ea9d2ccb39b745bf7dd8020a
Hevellyn De Souza <hevellynalves09@gmail.com>
2025-11-06 19:33:46 -0300

    Adicionando novos métodos. Resolved #1945

---
+++ Hevellyn/GerenciadorCliente.java
@@ -0,0 +1,30 @@
+public class GerenciadorCliente;
+
+    public void inserir(Cliente cliente) {
+        clientes.add(cliente);
+        System.out.println("Inserido!");
+    }
+
+    public Cliente consultar(int id) {
+        for (Cliente c : clientes) {
+            if (c.getId() == id) {
+                return c;
+            }
+        }
+        return null; 
+    }
+    public void remover(int id) {
+        Cliente cli = consultar(id);
+        if (c != null) {
+            clientes.remove(cli);
+        }
+    }
+
+    public void alterar(int id, String nvNome, String Email) {
+        Cliente cli = consultar(id);
+        if (cli != null) {
+            c.setNome(nvNome);
+            c.setEmail(Email);
+    
+        }
+    }
