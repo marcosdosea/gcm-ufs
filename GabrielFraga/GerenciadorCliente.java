9f5d91fa78a7c2197683fbf6002a9f343bd4a97c
Gabr1elFraga <gabrielsilveirafraga@hotmail.com>
2025-11-07 23:03:49 -0300

    Feature GerenciadorCliente. Closed #1973

---
+++ GabrielFraga/GerenciadorCliente.java
@@ -0,0 +1,11 @@
+package GabrielFraga;
+
+public class GerenciadorCliente {
+
+    public void inserirCliente(Cliente cliente) {
+        
+    }   
+    public void consultarCliente(String nome) {
+        
+    }
+}
