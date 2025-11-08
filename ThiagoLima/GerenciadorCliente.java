ce8079b57e40759e16282384e26ece065bf0d2c6
Thiago Lima <thi4got9lima@gmail.com>
2025-11-07 21:55:27 -0300

    Adicionado os métodos
    Métodos removerCliente e alterarCliente adicionados
    Resolve #1960

---
+++ ThiagoLima/GerenciadorCliente.java
@@ -0,0 +1,19 @@
+public package ThiagoLima;
+
+class GerenciadorCliente {
+
+    public void inserirCliente(Cliente cliente) {
+        
+    }
+
+    public Cliente consultarCliente(int id) {
+        
+    }
+    public void removerCliente(int id) {
+
+    }
+
+    public void alterarCliente(int id, String Nome, String Email) {
+        
+    }
+}
