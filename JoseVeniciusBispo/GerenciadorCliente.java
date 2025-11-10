37d5894b925a1fa77e84c16efa7aa6aa7d72ab73
JVB.viny <124806745+Jviniciusbl@users.noreply.github.com>
2025-11-10 07:15:15 -0300

    Criação dos metodos necessarios para a classe cliente Resolved #1992

--- JoseVeniciusBispo/GerenciadorCliente.java
+++ JoseVeniciusBispo/GerenciadorCliente.java
@@ -1,1 +1,23 @@
-ea
+import java.util.HashMap;
+import java.util.Map;
+
+public class GerenciadorCliente {
+    private Map<Integer, String> clientes;
+
+    public GerenciadorCliente() {
+        clientes = new HashMap<>();
+    }
+
+    public void inserir(int id, String nome) {
+        clientes.put(id, nome);
+        System.out.println("Cliente inserido com sucesso: " + nome);
+    }
+
+    public String consultar(int id) {
+        if (clientes.containsKey(id)) {
+            return "Cliente encontrado: " + clientes.get(id);
+        } else {
+            return "Cliente não encontrado.";
+        }
+    }
+}
