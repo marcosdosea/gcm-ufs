e518e30636f31d30a05193f06a1546f1764b0dd1
Gustavo-Correia <gucorreia2901@gmail.com>
2025-11-10 07:53:48 -0300

    Adicionado comentarios Closed #1995

--- LuisGustavoAlvesCorreia/GerenciadorCliente.java
+++ LuisGustavoAlvesCorreia/GerenciadorCliente.java
@@ -5,12 +5,12 @@ public class GerenciadorCliente {
 
     private List<Cliente> clientes = new ArrayList<>();
 
-    
+    //metodo para inserir
     public void inserir(Cliente cliente) {
         clientes.add(cliente);
     }
 
-    
+    //metodo para consultar
     public Cliente consultar(int id) {
         for (Cliente c : clientes) {
             if (c.getid() == id) {
