8f82a662a6f7fe12054fcab507619da91d805b74
Lorena Pereira Nascimento <lorena.lindaflower@gmail.com>
2025-11-10 14:19:48 -0300

    Cadastro e consulta implementados. close #2003

---
+++ Lorena/Cliente.java
@@ -0,0 +1,31 @@
+public class Cliente {
+    private int id;
+    private String nome;
+    private String email;
+
+    public Cliente(int id, String nome, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+}
