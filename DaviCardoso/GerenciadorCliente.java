423ee7b2e203922725f3dc7f3009c8679180ebd6
Davi Cardoso <122702586+davicardoso5524@users.noreply.github.com>
2025-11-10 21:59:45 -0300

    Revert "Atualização na classe GerenciadoCliente e foram criados os metódos necessários"

--- DaviCardoso/GerenciadorCliente.java
+++ DaviCardoso/GerenciadorCliente.java
@@ -16,21 +16,3 @@ public class GerenciadorCliente {
             }
         }
     }
-
-    public void removerCliente(String nome) {
-        if (clientes.remove(nome)) {
-            System.out.println("Cliente \"" + nome + "\" removido com sucesso!");
-        } else {
-            System.out.println("Cliente \"" + nome + "\" não encontrado.");
-        }
-    }
-
-    public void alterarCliente(String nomeAntigo, String nomeNovo) {
-        int indice = clientes.indexOf(nomeAntigo);
-        if (indice != -1) {
-            clientes.set(indice, nomeNovo);
-            System.out.println("Cliente \"" + nomeAntigo + "\" alterado para \"" + nomeNovo + "\" com sucesso!");
-        } else {
-            System.out.println("Cliente \"" + nomeAntigo + "\" não encontrado.");
-        }
-    }
