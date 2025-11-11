24d84b8f86ee30c67b63d32d1c035cc71ca9cb67
LucasCunhaAlmeida <lucascunhaalmeida2003@gmail.com>
2025-11-10 22:10:24 -0300

    Métodos feitos. closes #2022

---
+++ LucasCunhaAlmeida/GerenciadorCliente .java
@@ -0,0 +1,18 @@
+public class GerenciadorCliente {
+
+    public void inserir(){
+        
+    }
+
+    public void consultar(){
+        
+    }
+
+    public void remover(){
+        
+    }
+
+    public void alterar(){
+        
+    }
+}
