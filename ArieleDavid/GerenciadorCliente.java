faca36f8c96940f86df04a6d5dc759d3358e2fd3
David Santos <davidsantos8794@gmail.com>
2026-08-30 15:55:19 -0300

    Adiciona metodo cadastrarCliente

--- ArieleDavid/GerenciadorCliente.java
+++ ArieleDavid/GerenciadorCliente.java
@@ -3,4 +3,8 @@ public class GerenciadorCliente {
     public static void main(String[] args) {
         System.out.println("Gerenciador de Clientes");
     }
+
+    public static void cadastrarCliente() {
+        System.out.println("Cliente cadastrado com sucesso!");
+    }
 }
