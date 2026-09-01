3420345f17a191e5492c782730799f795f027458
Ulisses Dorenski Lima Ribeiro <202400008059@office.ufs.br>
2026-09-01 10:40:29 -0300

    Adicionou método atualizar

--- FelipeUlisses/GerenciadorCliente.java
+++ FelipeUlisses/GerenciadorCliente.java
@@ -4,4 +4,8 @@ public class GerenciadorCliente {
         // Lógica para inserir o cliente no banco de dados
     }
 
+    public void atualizar(Cliente cliente) {
+        // Lógica para atualizar o cliente no banco de dados
+    }
+
 }
