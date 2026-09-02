438280c479541d7e4c994a0836b5bb194adc40e9
Pauloisc <pauloitalo061@gmail.com>
2026-09-01 21:39:51 -0300

    Implementando classe

--- PauloPedro/GerenciadorCliente.java
+++ PauloPedro/GerenciadorCliente.java
@@ -2,17 +2,13 @@ public class GerenciadorCliente{
     private List<Cliente> clientes = new ArrayList<>();
 
     public void inserir(Cliente cliente){
-        Cliente novoCliente = new Cliente(cliente);
-        clientes.add(novoCliente);
+        clientes.add(cliente);
         System.out.println("Cliente cadastrado!");
     }
 
-    public void atualizar(Cliente cliente) {
-        for (Cliente c : clientes) {
-            if (c.getId() == cliente.getId()) {
-                c.atualizar(cliente);
-                break;       
-            }
-        }
+    public void atualizar(Cliente cliente, String nome, String CPF) {
+        cliente.nome = nome;
+        cliente.cpf = cpf;
+        System.out.println("Cliente atualizado!")
     }
 }
