23a238034f173bf225faada1cff8bbcd9482da62
Bernardo Santos <bernardonx20@gmail.com>
2026-09-03 10:13:30 -0300

    Atualização do arquivo

--- BernardoIgorlemos/GerenciadorCliente.java
+++ BernardoIgorlemos/GerenciadorCliente.java
@@ -5,17 +5,11 @@ public class GerenciadorCliente{
   private String nome;
   private int idade;
   private String dataNascimento;
-<<<<<<< HEAD
+  
   private ArrayList <Cliente> listclient = new ArrayList<>(); 
   public void criarCliente(String nome, int id, String dataNascimento){
    listclient.add(new Cliente(nome, id, dataNascimento));
 
-=======
-
-  public void criarCliente(String nome, int id, String dataNascimento){
-    //criador cliente
->>>>>>> 8bee4f9ac8a98a6a755d6509fd3035de395209bc
-  }
   public void atualizarCliente(String nome, int id, String dataNascimento){
     //atualizar cliente
   }
