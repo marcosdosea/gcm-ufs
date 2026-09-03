1bdc355beeff22754096868debc6c0587e894fb5
bernardonx20-dot <bernardonx20@gmail.com>
2026-09-03 10:04:22 -0300

    Atualização inserir cliente arraylist

--- BernardoIgorlemos/GerenciadorCliente.java
+++ BernardoIgorlemos/GerenciadorCliente.java
@@ -1,10 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class GerenciadorCliente{
   private String nome;
   private int idade;
   private String dataNascimento;
-
-  public Cliente criarCliente(String nome, int id, String dataNascimento){
-   return new Cliente(nome, id, dataNascimento); 
+  private ArrayList <Cliente> listclient = new ArrayList<>(); 
+  public void criarCliente(String nome, int id, String dataNascimento){
+   listclient.add(new Cliente(nome, id, dataNascimento));
 
   }
   public void atualizarCliente(String nome, int id, String dataNascimento){
