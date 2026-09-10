9dc2da54152c870f41648a0bcc2bdeb1ee106f31
Guilherme <guilheeme1108@gmail.com>
2026-09-10 10:19:22 -0300

    atualizando para retirar erros #2042

--- DaianeGuilherme/NovaBranch.java
+++ DaianeGuilherme/NovaBranch.java
@@ -1,8 +1,2 @@
 public class Branch_Cliente {
-    public void Inserir(Cliente cliente){
-        salvarCliente(cliente);
-    }
-    public void Remover(Cliente cliente){
-        deleteCliente(cliente);
-    }
 }
