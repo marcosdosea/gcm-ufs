f9a7c639b3154c4548d28f1bf435069883f3f6cd
Guilherme <guilheeme1108@gmail.com>
2026-09-10 09:56:19 -0300

    criacao metodo Remover Cliente #2042

--- DaianeGuilherme/NovaBranch.java
+++ DaianeGuilherme/NovaBranch.java
@@ -2,3 +2,7 @@ public class Branch_Cliente {
     public void Inserir(Cliente cliente){
         salvarCliente(cliente);
     }
+    public void Remover(Cliente cliente){
+        salvarCliente(cliente);
+    }
+}
