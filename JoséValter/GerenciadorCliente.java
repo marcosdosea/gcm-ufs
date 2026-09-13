6752d15604524c23fab743b1e3f1ad745644ec5c
ValterSantos1 <jvaltersantos80@gmail.com>
2026-09-13 19:49:02 -0300

    adiçãod dos métodos alterar e remover na classe de GerenciadorCliente. Resolve #2169

---
+++ JoséValter/GerenciadorCliente.java
@@ -0,0 +1,11 @@
+package JoséValter;
+
+public class GerenciadorCliente {
+    public void inserir(Cliente c){}
+    
+    public Cliente consultar(String cpf){}
+
+    public Cliente alterar(String cpf){}
+
+    public void remover(String cpf){}
+}
