dd79363fc5722746aca76a783bcf41b13c0eed49
felipe <felipe12oliveiraa@gmail.com>
2026-09-14 08:55:26 -0300

    Criação métodos remover e alterar. #2177

--- FelipeOliveira/GerenciadorCliente.java
+++ FelipeOliveira/GerenciadorCliente.java
@@ -7,4 +7,12 @@ public class GerenciadorCliente {
     public void consultar(Cliente cliente) {
         // Lógica para consultar o cliente no banco de dados
     }
+
+     public void remover(Cliente cliente) {
+        // Lógica para remover o cliente no banco de dados
+    }
+    
+    public void alterar(Cliente cliente) {
+        // Lógica para alterar o cliente no banco de dados
+    }
 }
