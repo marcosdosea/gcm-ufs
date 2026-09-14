53ed62f1548e2291dd324a64b3e3bf7d2bb99b82
Dorenski <jpulisse@gmail.com>
2026-09-14 09:54:52 -0300

    Foi criada a classe GerenciadorCliente com método de inserir e consultar

---
+++ UlissesDorenski/GerenciadorCliente.java
@@ -0,0 +1,9 @@
+public class GerenciadorCliente {
+
+    public void inserir(Cliente cliente) {
+        // Insere o cliente
+    }
+    
+    public void consultar(Cliente cliente) {
+        // Consulta o cliente
+    }
