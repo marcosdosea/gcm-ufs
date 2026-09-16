a6a21bac4a0853c6d9342a1725dc04d05706a002
Pedro Henrique <peedrohs.mesquita@outlook.com>
2026-09-16 20:20:20 -0300

    Implementação do Gerenciador de Venda, permitindo finalizar e cancelar venda. Issue #2204.

---
+++ PedroMesquita/GerenciadorVenda.java
@@ -0,0 +1,9 @@
+public class GerenciadoVenda {
+    public void finalizarVenda(Venda v) {
+        // Implementação do método para finalizar a venda
+    }
+
+    public void cancelarVenda(Venda v) {
+        // Implementação do método para cancelar a venda
+    }
+}
