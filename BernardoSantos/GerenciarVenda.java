f95832de1d977b849594ff87447723aa781aa0e8
bernardonx20-dot <bernardonx20@gmail.com>
2026-09-17 10:52:25 -0300

    Correção nome da funcionabilidade

--- BernardoSantos/GerenciarVenda.java
+++ BernardoSantos/GerenciarVenda.java
@@ -1,8 +1,8 @@
 package gcm-ufs.BernardoSantos;
 
 public class GerenciarVenda {
-  public int cancelarVenda(int id){
-    // cancela a venda no banco de dados pelo id e retorna o id da venda cancelada
+  public int FinalizarVenda(int id){
+    // Finaliza a venda no banco de dados pelo id e retorna o id da venda finalizada
     return id;
   }
   public int cancelarVenda(int id, String motivo){
