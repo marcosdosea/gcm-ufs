d7fc1649eadc873fe0b6f5bcebb1e3859f2867fc
daianesnts <daidai.santos@hotmail.com>
2026-09-16 22:20:46 -0300

    Adiciona métodos para remover e alterar cliente na classe que gerencia cliente

--- DaianeSantos/GerenciadorCliente.java
+++ DaianeSantos/GerenciadorCliente.java
@@ -22,4 +22,25 @@ public class GerenciadorCliente {
         System.out.println("Cliente consultado: " + clientes[posicao]);
         return clientes[posicao];
     }
+
+        public void remover(int posicao) {
+        if (posicao < 0 || posicao >= quantidade) {
+            System.out.println("Cliente não encontrado");
+            return;
+        }
+        System.out.println("Cliente removido: " + clientes[posicao]);
+        for (int i = posicao; i < quantidade - 1; i++) {
+            clientes[i] = clientes[i + 1];
+        }
+        quantidade--;
+    }
+
+    public void alterar(int posicao, String novoCliente) {
+        if (posicao < 0 || posicao >= quantidade) {
+            System.out.println("Cliente não encontrado");
+            return;
+        }
+        System.out.println("Cliente alterado de '" + clientes[posicao] + "' para '" + novoCliente + "'");
+        clientes[posicao] = novoCliente;
+    }
 }
