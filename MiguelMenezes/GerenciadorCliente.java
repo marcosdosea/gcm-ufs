6b17935e866f28e9b84abad567646759d4c28042
Zerxf-exe <miguelmenezes90@outlook.com>
2026-09-16 22:34:17 -0300

    Adicionado os metodos remover e alterar na classe gerenciadorcliente Issue #2232

---
+++ MiguelMenezes/GerenciadorCliente.java
@@ -0,0 +1,21 @@
+package MiguelMenezes;
+
+public class GerenciadorCliente {
+
+    public void inserir(Cliente c){
+
+    }
+
+    public void consultar(Cliente c){
+
+    }
+
+    public void remover(Cliente c){
+
+    }
+
+    public void alterar(Cliente c){
+        
+    }
+    
+}
