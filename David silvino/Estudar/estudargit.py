71cb5b7db77ae2913f4a6f0721e404d2ce5903bd
David Santos <davidsantos8794@gmail.com>
2026-09-19 20:23:45 -0300

    métodos feitos

--- David silvino/Estudar/estudargit.py
+++ David silvino/Estudar/estudargit.py
@@ -10,7 +10,13 @@ valor2 = float(input("Segundo valor:"))
 
 operacao = int(input("Digite a operacao da calculadora:"))
 
-
-
+if (operacao == 1):
+    print(f"Adição: {valor1 + valor2}")
+if (operacao == 2):
+    print(f"subtração:  {valor1 - valor2}")
+if (operacao == 3):
+    print(f"multiplicação:  {valor1 * valor2}") 
+if (operacao == 4):
+    print(f"Adidivisãoção:  {valor1 / valor2}")
 
 
